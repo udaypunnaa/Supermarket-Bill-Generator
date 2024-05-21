2d5671f48068199af77b417744d5c2de8e1e2c8f
Uday Punna <udaypunna1807@gmail.com>
2024-05-21 17:19:44 +0530

    Add files via upload

---
+++ bill.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter Drinks price=");
+    scanf("%d",&x);
+    printf("Enter Ice Cream price=");
+    scanf("%d",&y);
+    printf("\t\t\t-------SMART POINT-------\n");
+    printf("\t\t\t Reliance Retail Limited\n");
+    printf("\t\t\t   RELIANCE SMART POINT\n");
+    printf("\t\t\t Plot No.40(Western Part)\n");
+    printf("\t\t\t   covered by SY.No.407\n");
+    printf("\t\t\t\t Keesara Mandal\n");
+    printf("\t\t\tMedchal malkajgiri District\n");
+    printf("\t\t\tHyderabad..Telangana-501301\n");
+    printf("\t\tCustomer care 18008910001/18001027382\n");
+    printf("  \t\t   CIN No : U01100MH1999PLC120563\n");
+    printf("\t\t\t  GSTIN-36AABCR1718E1ZQ\n");
+    printf("\t\t  FSSAI Lic No - 13620034000393\n");
+    printf("----------------------------------------------------\n  You have saved Rs.x\n----------------------------------------------------\n");
+    printf("Place of Supply & State Code 36 16\n");
+    printf("Customer Type : URD\n");
+    printf("\t\t\tTAX INVOICE\n");
+    printf("******Original for Recipient*******\n");
+    printf("Date:01-01-2024\t Bill No:92\n");
+    printf("Store 0329  Cashier :60643253\n");
+    printf("----------------------------------------------------\nHSN Code   Item Description  Net price   Qty   Value\n----------------------------------------------------\n");
+    printf("1)CGST $ 2.50  SGST $ 2.50 \n");
+    printf("04039090\t\tDRINKS\t\n");
+    printf(" \t\t\t\n");
+    printf("2)CGST $ 2.50  SGST $ 2.50 \n");
+    printf("04039091\t\tICE CREAMS\t\n");
+    printf("----------------------------------------------------\n");
+    printf("Items:2\nTotal Amount paid\nNet Amount Paid in CASH\nChange\n(AMOUNT INCLUSIVE OF APPLICABLE TAXES)\n");
+    printf("----------------------------------------------------\n    ----GST Breakup Deta:15----Amount(INR)----\n----------------------------------------------------\n");
+    printf("GST   Taxable   CGST   SGST   CESS   Total Amount\nIND   Amount\n");
+    printf("----------------------------------------------------\n 1)\n----------------------------------------------------\n");
+    printf(" 2)\n----------------------------------------------------\n");
+    printf("Total\n----------------------------------------------------\n");
+    printf("PaymentRefNo# U328106009230122094\nTaxInvoice# U323791279398274\nPone Card No.:76*******343\nTerms & Conditions Apply");
+    printf("\n----------------------------------------------------\n");
+    printf("   *******Thank You for Shopping With us******** ");
+    return 0;
+}
